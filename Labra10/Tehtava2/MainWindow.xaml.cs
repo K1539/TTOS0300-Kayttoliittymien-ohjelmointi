0152e1b77941b185d5a2982d96db34ecf9257364
Jere <Jere@lppri>
2017-02-11 22:03:59 +0200

    t2

--- Labra10/Tehtava2/MainWindow.xaml.cs
+++ Labra10/Tehtava2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace Tehtava2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(txtbLogin.Text);
+            Debug.WriteLine(txtbPassword.Text);
         }
     }
 }
